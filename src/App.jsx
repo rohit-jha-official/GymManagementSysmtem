99add78c404f3d5f9bc817c5fdaba444a8881aea
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2025-12-25 11:25:44 +0530

    side nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import SideNavBar from "./components/SideNavBar/SideNavBar";
+import SideNavBar from "./components/SideNavbar/SideNav";
 
 function App() {
   return (
--- src/components/SideNavbar/SideNav.jsx
+++ src/components/SideNavbar/SideNav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./SideNavBar.css";
+import "./SideNav.css";
 import {
   FaHome,
   FaUsers,

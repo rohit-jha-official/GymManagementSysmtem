61babcde872360122a9d2c52d7849311fd4e3f02
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2025-12-26 18:55:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./components/Dashboard/Dashboard";
 import AllMembers from "./components/All Member/AllMember";
-import Cardlist from "./components/RFIDcards/Cardlist";
-import Replace from "./components/RFIDcards/Replace";
 
 function App() {
   return (
@@ -16,8 +14,6 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/members" element={<AllMembers />} />
-          <Route path="/rfid" element={<Cardlist />} />
-          <Route path="/rfid/replace" element={<Replace />} />
         </Route>
       </Routes>
     </BrowserRouter>

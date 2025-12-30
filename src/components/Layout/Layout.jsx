ed2eb74899db428fae225164be1d8bc218c763fb
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2025-12-30 13:11:43 +0530

    navbar, topbar and dashbord responsive

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import SideNavBar from "../SideNavbar/SideNav";
-import Topbar from "../Topbar/Topbar";
+import Topbar from "../TopBar/topbar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="dashboard">
-      <SideNavBar />
+      {/* SIDEBAR */}
+      <SideNavBar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+
+      {/* MAIN CONTENT */}
       <div className="dashboard-main">
-        <Topbar />
+        <Topbar toggleSidebar={() => setSidebarOpen(true)} />
+
         <div className="dashboard-content">
           <Outlet />
         </div>

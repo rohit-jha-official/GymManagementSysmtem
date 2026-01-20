a4ef344f88d957005baa507ab6977067a8ce43a2
siddhisingh0902-pixel <siddhisingh0902@gmail.com>
2026-01-20 20:57:49 +0530

    Update Middleauthware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -13,7 +13,7 @@ export const protect = (req, res, next) => {
    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 req.user = {
-  id: decoded.adminId,
+  id: decoded.Id,
   branchId: decoded.branchId, // 🔥 ADD THIS
 };
 

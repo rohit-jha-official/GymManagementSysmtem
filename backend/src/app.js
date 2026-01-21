d6afef745622314d8614d18e350b91be31dd6978
siddhisingh0902-pixel <siddhisingh0902@gmail.com>
2026-01-22 02:22:49 +0530

    Render7

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,14 @@ const app = express();
 /* ================================
    MIDDLEWARE
    ================================ */
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "https://wellnessgymexpressbelur.netlify.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 app.use("/api/admin", adminRoutes);

b7a5d0af4592b212103a6c16d6f641f634c66e5d
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2026-01-23 01:37:22 +0530

    name updated

--- src/components/RecentActivity/Recentactivity.jsx
+++ src/components/RecentActivity/Recentactivity.jsx
@@ -1,4 +1,4 @@
-import "./RecentActivity.css";
+import "./Recentactivity.css";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 import {
@@ -42,7 +42,7 @@ const getTypeClass = (type) => {
   }
 };
 
-const RecentActivity = () => {
+const Recentactivity = () => {
   const [activities, setActivities] = useState([]);
 
   /* 🔹 LOAD ACTIVITIES (JWT SAFE) */
@@ -110,4 +110,4 @@ const RecentActivity = () => {
   );
 };
 
-export default RecentActivity;
+export default Recentactivity;

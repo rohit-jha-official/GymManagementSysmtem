90c447c680b9c41a2d8d32b1e9116561a42ef6d8
siddhisingh0902-pixel <siddhisingh0902@gmail.com>
2026-01-23 02:42:52 +0530

    F01

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: "http://localhost:5001/api",
+   baseURL: "https://gym-backend-render-okpo.onrender.com",
 });
 
 /* 🔐 AUTO ATTACH JWT TOKEN (SAFE) */
